0186392970e92c38cf43647c893e6ee532eb723c
ali <khakbazan.io@proton.me>
2025-03-21 18:57:23 +0330

    feat: rendering coins list component in home page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,13 @@
+import { CoinsList } from "@/components/coins";
+import { getCoinsList } from "@/models/coins";
 import { Fragment } from "react";
 
 export default async function HomePage() {
+  const coinsList = await getCoinsList();
+
   return (
     <Fragment>
-      <p>Welcome...</p>
+      <CoinsList coins={coinsList?.result?.markets} />
     </Fragment>
   );
 }
