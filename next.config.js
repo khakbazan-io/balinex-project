338596ac7f8528587380bcc54cddf1c03c901bda
ali <khakbazan.io@proton.me>
2025-03-21 18:51:17 +0330

    chore: remote pattern added for Image cm and some colors in tailwind config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.wallex.ir",
+      },
+    ],
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
