23d873465b88d82cf11646050c3ccf6a50c8fe5f
ali <khakbazan.io@proton.me>
2025-03-23 01:16:57 +0330

    refactor: id removed from deps list

--- core/hooks/usePagination.ts
+++ core/hooks/usePagination.ts
@@ -73,7 +73,7 @@ export function usePagination({
     if (savedPage !== undefined) {
       setCurrentPage(savedPage);
     }
-  }, [id, savedPage]);
+  }, [savedPage]);
 
   return {
     currentPage,
