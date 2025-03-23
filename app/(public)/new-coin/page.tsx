3dbe2033c6a1603f1c226acfa4d0f25af3d449b9
ali <khakbazan.io@proton.me>
2025-03-24 01:10:23 +0330

    chore: metadata for new coin page

--- app/(public)/new-coin/page.tsx
+++ app/(public)/new-coin/page.tsx
@@ -1,4 +1,13 @@
 import { NewCoin } from "@/components/coins";
+import { generateMeta } from "@/core/utils";
+import type { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  return generateMeta({
+    title: "افزودن رمزارز جدید",
+    description: "افزودن رمزارز جدید",
+  });
+}
 
 export default async function NewCoinPage() {
   return (
